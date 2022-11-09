5e90e87fdb5280ed5f7d1bc783b8e80d79b601a7
Gabriela Aguilar <gabrielamoreno00.01@gmail.com>
2022-11-09 10:40:00 -0400

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+import {Todo, TodoList} from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+export const todoList = new TodoList();
+
+
+todoList.todos.forEach(crearTodoHtml);
